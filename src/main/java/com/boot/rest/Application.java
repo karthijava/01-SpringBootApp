f5e98bf7e2b231aca65097d15e8df51a4d1a357f
karthijava <karti497@gmail.com>
2021-11-12 20:39:11 +0530

    app class is modified

--- src/main/java/com/boot/rest/Application.java
+++ src/main/java/com/boot/rest/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+	private String email;
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = 
 			SpringApplication.run(Application.class, args);
